944fdf09322454def02dc20bb09ab0d7913561e5
백주원 <ghtghtep@gmail.com>
2023-04-12 02:15:24 +0000

    웹 프로그래밍 6주차

--- Js/search.js
+++ Js/search.js
@@ -2,7 +2,7 @@ document.getElementById("search_btn").addEventListener('click', search_message);
 
 function search_message(){
 	alert("검색을 수행한다!");
-	let search str = document.querySelector("#search_txt");
+	let search_str = document.querySelector("#search_txt");
 	document.getElementById("search_message").innerHTML = search_str.value;
 	console.log(search_str.value);
 }

e6abcdd1543c1f869f450e65d6cc4f4c48500720
백주원 <ghtghtep@gmail.com>
2023-05-03 02:21:23 +0000

    웹 프로그래밍 9주차 실습

--- Js/search.js
+++ Js/search.js
@@ -1,9 +1,40 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
+let search_array = [];
+
 function search_message(){
 	alert("검색을 수행한다!");
 	let search_str = document.querySelector("#search_txt");
-	document.getElementById("search_message").innerHTML = search_str.value;
-	console.log(search_str.value);
+	if(search_str.value.length === 0){ // 문자 길이, 엄격한 비교
+       alert("검색어가 비었습니다. 입력해주세요"); 
+    }
+	
+	else if(no_str(search_str.value))
+		{
+			alert("뗵");
+		}
+    else{
+       alert("검색을 수행합니다!");
+		if(search_array.length >= 10){
+		  search_array.shift();	
+		}
+		search_array.push(search_str.value);
+       let text = document.getElementById("search_message").innerHTML = search_array.toString();
+       document.querySelector("#form_main").submit();
+    }
 }
 
+
+function no_str(input)
+{
+	let tlqkf = ["시발", "혐", "좆"];
+	
+	for(let whw of tlqkf){
+	if(input == whw)
+		{
+			return true;
+		}
+
+	}
+	return false;
+}

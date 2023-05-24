97cbb9feb4d6ebe3e06bd290cba8e1a9d7f0b283
백주원 <ghtghtep@gmail.com>
2023-05-24 02:26:47 +0000

    웹 프로그래밍 12주차

--- Js/session.js
+++ Js/session.js
@@ -1,8 +1,18 @@
 function session_set() { //세션 저장
     let id = document.querySelector("#floatingInput");
-    if (sessionStorage) {
+    let password = document.querySelector("#floatingPassword");
+    let random = new Date(); // 랜덤 타임스탬프
+    
+    const obj = { // 객체 선언
+    id : id.value,
+    otp : random
+    }
+	
+	if (sessionStorage) {
         sessionStorage.setItem("Session_Storage_test", id.value);
-
+        let en_text = encrypt_text(objString); // 암호화
+        sessionStorage.setItem("Session_Storage_object", objString);
+        sessionStorage.setItem("Session_Storage_encrypted", en_text);
     } else {
         alert("로컬 스토리지 지원 x");
     }
@@ -10,7 +20,7 @@ function session_set() { //세션 저장
 
 function session_get() { //세션 읽기
     if (sessionStorage) {
-       return sessionStorage.getItem("Session_Storage_test");
+       return sessionStorage.getItem("Session_Storage_encrypted");
     } else {
         alert("세션 스토리지 지원 x");
     }
